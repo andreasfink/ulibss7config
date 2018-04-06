cd863b9d3f4d5f52e32df374b096287e2c771a9c
Andreas Fink <afink@amdtelecom.net>
2018-04-06 12:47:23 +0300

    cleaned up project file

--- ulibss7config/ulibss7config.h
+++ ulibss7config/ulibss7config.h
@@ -10,12 +10,45 @@
 
 #import "UMSS7ConfigAppDelegateProtocol.h"
 
-//! Project version number for ulibss7config.
-FOUNDATION_EXPORT double ulibss7configVersionNumber;
-
-//! Project version string for ulibss7config.
-FOUNDATION_EXPORT const unsigned char ulibss7configVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ulibss7config/PublicHeader.h>
-
-
+#import "UMSS7ConfigObject.h"
+#import "UMSS7ConfigMacros.h"
+#import "UMSS7ConfigStorage.h"
+#import "UMSS7ConfigGeneral.h"
+#import "UMSS7ConfigSyslogDestination.h"
+#import "UMSS7ConfigWebserver.h"
+#import "UMSS7ConfigTelnet.h"
+#import "UMSS7ConfigUser.h"
+#import "UMSS7ConfigDatabasePool.h"
+#import "UMSS7ConfigSCTP.h"
+#import "UMSS7ConfigM2PA.h"
+#import "UMSS7ConfigMTP3.h"
+#import "UMSS7ConfigMTP3Link.h"
+#import "UMSS7ConfigMTP3Linkset.h"
+#import "UMSS7ConfigM3UAAS.h"
+#import "UMSS7ConfigM3UAASP.h"
+#import "UMSS7ConfigMTP3Filter.h"
+#import "UMSS7ConfigMTP3FilterEntry.h"
+#import "UMSS7ConfigMTP3Route.h"
+#import "UMSS7ConfigSCCP.h"
+#import "UMSS7ConfigSCCPTranslationTable.h"
+#import "UMSS7ConfigSCCPTranslationTableEntry.h"
+#import "UMSS7ConfigSCCPDestination.h"
+#import "UMSS7ConfigSCCPDestinationEntry.h"
+#import "UMSS7ConfigSCCPFilter.h"
+#import "UMSS7ConfigSCCPFilterEntry.h"
+#import "UMSS7ConfigTCAP.h"
+#import "UMSS7ConfigTCAPFilter.h"
+#import "UMSS7ConfigTCAPFilterEntry.h"
+#import "UMSS7ConfigGSMMAP.h"
+#import "UMSS7ConfigGSMMAPFilter.h"
+#import "UMSS7ConfigGSMMAPFilterEntry.h"
+#import "UMSS7ConfigSMS.h"
+#import "UMSS7ConfigSMSFilter.h"
+#import "UMSS7ConfigSMSFilterEntry.h"
+#import "UMSS7ConfigHLR.h"
+#import "UMSS7ConfigMSC.h"
+#import "UMSS7ConfigSMSC.h"
+#import "UMSS7ConfigSMSProxy.h"
+#import "UMSS7ConfigCdrWriter.h"
+#import "UMSS7ApiTaskAll.h"
+#import "UMSS7ApiSession.h"
