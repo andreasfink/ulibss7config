95501bb2953de955e17eca60568a08246ddba633
Andreas Fink <andreas@fink.org>
2018-07-26 19:10:40 +0200

    make ulibdb harmonized with ulibss7config

--- ulibss7config/Config/UMSS7ConfigDatabasePool.h
+++ ulibss7config/Config/UMSS7ConfigDatabasePool.h
@@ -10,10 +10,32 @@
 
 @interface UMSS7ConfigDatabasePool : UMSS7ConfigObject
 {
+    NSString *_host;
+    NSString *_databaseName;
+    NSString *_driver;
+    NSString *_user;
+    NSString *_pass;
+    NSNumber *_port;
+    NSNumber *_minSessions;
+    NSNumber *_maxSessions;
+    NSString *_socket;
+    NSNumber *_pingIntervall;
+    NSString *_storageType;
+    NSString *_version;
 }
 
-@property(readwrite,strong,atomic)  NSString *attachTo;
-
+@property(readwrite,strong,atomic)  NSString *host;
+@property(readwrite,strong,atomic)  NSString *databaseName;
+@property(readwrite,strong,atomic)  NSString *driver;
+@property(readwrite,strong,atomic)  NSString *user;
+@property(readwrite,strong,atomic)  NSString *pass;
+@property(readwrite,strong,atomic)  NSNumber *port;
+@property(readwrite,strong,atomic)  NSNumber *minSessions;
+@property(readwrite,strong,atomic)  NSNumber *maxSessions;
+@property(readwrite,strong,atomic)  NSString *socket;
+@property(readwrite,strong,atomic)  NSNumber *pingIntervall;
+@property(readwrite,strong,atomic)  NSString *storageType;
+@property(readwrite,strong,atomic)  NSString *version;
 
 + (NSString *)type;
 - (NSString *)type;
