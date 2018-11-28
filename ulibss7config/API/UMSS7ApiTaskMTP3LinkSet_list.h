66b09d1e78bc348f1b9653e58e0036e46267f96b
Andreas Fink <afink@amdtelecom.net>
2018-11-28 16:31:39 +0200

    missing file

---
+++ ulibss7config/API/UMSS7ApiTaskMTP3LinkSet_list.h
@@ -0,0 +1,13 @@
+//
+//  UMSS7ApiTaskMTP3LinkSet_list.h
+//  estp
+//
+//  Created by Andreas Fink on 13.03.18.
+//  Copyright © 2018 Andreas Fink. All rights reserved.
+//
+
+#import "UMSS7ApiTask.h"
+
+@interface UMSS7ApiTaskMTP3LinkSet_list : UMSS7ApiTask
+
+@end
