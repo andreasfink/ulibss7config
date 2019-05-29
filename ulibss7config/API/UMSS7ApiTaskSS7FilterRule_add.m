8982d35fa2f22346cba5bea9b5f2b0cc3377f05a
Stathis Andronikos <sandronikos@ss7devsrv.amdtelecom.net>
2019-05-29 17:38:30 +0300

    Implementing Addition of a filtering Rule

--- ulibss7config/API/UMSS7ApiTaskSS7FilterRule_add.m
+++ ulibss7config/API/UMSS7ApiTaskSS7FilterRule_add.m
@@ -7,9 +7,11 @@
 //
 
 #import "UMSS7ApiTaskSS7FilterRule_add.h"
+#import "UMSS7ConfigAppDelegateProtocol.h"
+#import "UMSS7ConfigStorage.h"
 #import "UMSS7ConfigObject.h"
 #import "UMSS7ConfigSS7FilterRule.h"
-#import "UMSS7ConfigStorage.h"
+
 
 @implementation UMSS7ApiTaskSS7FilterRule_add
 
@@ -32,7 +34,32 @@
         [self sendErrorNotAuthorized];
         return;
     }
-    [self sendErrorNotImplemented];
+	
+	UMSS7ConfigStorage *cs = [_appDelegate runningConfig];
+	UMSS7ConfigM2PA *stagingArea = NULL;
+    /* TODO : Add To AppDelegate a new method : getStagingArea
+		UMSS7ConfigM2PA *stagingArea = [cs getStagingArea:name];
+	*/
+	if(stagingArea == NULL)
+    {
+        [self sendErrorNotFound];
+    }
+    else
+    {
+		@try
+		{
+			UMSS7ConfigSS7FilterRule* filterRule  = [[UMSS7ConfigSS7FilterRule alloc]initWithConfig:_webRequest.params];
+			UMSynchronizedSortedDictionary *config = filterRule.config;
+			/* TODO : Add to Staging Area
+			[_appDelegate addFilterRuleToStagingArea:config.dictionaryCopy];
+			*/
+			[self sendResultObject:config];
+		}
+		@catch(NSException *e)
+		{
+			[self sendException:e];
+		}
+    }
 }
 
 @end
