dbe5ead438e1708ee1cb7eecfb7df0a24ce71703
root <root@ss7devsrv.amdtelecom.net>
2019-05-31 13:41:39 +0300

    Implementing Rule Status - jira181

--- ulibss7config/API/UMSS7ApiTaskSS7FilterRule_status.m
+++ ulibss7config/API/UMSS7ApiTaskSS7FilterRule_status.m
@@ -7,9 +7,13 @@
 //
 
 #import "UMSS7ApiTaskSS7FilterRule_status.h"
+#import "UMSS7ConfigAppDelegateProtocol.h"
+#import "UMSS7ConfigStorage.h"
 #import "UMSS7ConfigObject.h"
 #import "UMSS7ConfigSS7FilterRule.h"
-#import "UMSS7ConfigStorage.h"
+#import "UMSS7ConfigStagingAreaStorage.h"
+#import "UMSS7ConfigSS7FilterRuleset.h"
+#import "UMSS7ApiSession.h"
 
 @implementation UMSS7ApiTaskSS7FilterRule_status
 
@@ -31,7 +35,57 @@
         [self sendErrorNotAuthorized];
         return;
     }
-    [self sendErrorNotImplemented];
+    
+	// 1. Get Staging Area
+	UMSS7ConfigStagingAreaStorage *stagingArea = [_appDelegate getStagingAreaForSession:_apiSession.sessionKey];
+	if(stagingArea == NULL)
+    {
+        [self sendErrorNotFound];
+    }
+    else
+    {
+		@try
+		{
+			// 2. Get Engine  
+			NSString *engine_name = _webRequest.params[@"engine"];
+			UMPluginHandler *engine = [_appDelegate getSS7FilterEngineHandler:engine_name];
+			
+			// 3. Get Rule-Set 
+			NSString *ruleset_name = _webRequest.params[@"filter-ruleset"];
+			UMSS7ConfigSS7FilterRuleset* rSet = stagingArea.filter_rule_set_dict[ruleset_name];
+			
+			// 4. Get index of rule
+			NSString *idx = _webRequest.params[@"entry-nr"];
+			
+			// 5. Verify if engine, rule-set, rule exist
+			if(engine == NULL || rSet == NULL || idx == NULL)
+			{
+				// 5a. Not found
+				[self sendErrorNotFound];
+			}
+			else
+			{
+				// 5b. Get rule
+				NSInteger i = [idx integerValue];
+				UMSS7ConfigSS7FilterRule* filterRule = [rSet getRuleAtIndex:i];
+				if(filterRule == NULL)
+				{
+				    // 5b-1. Rule not found
+					[self sendErrorNotFound];
+				}
+				else
+				{
+					// 5b-2. OK
+					[self sendResultOK];
+				}
+			}
+		}
+		@catch(NSException *e)
+		{
+			[self sendException:e];
+		}
+    }
+	
 }
 
 @end
