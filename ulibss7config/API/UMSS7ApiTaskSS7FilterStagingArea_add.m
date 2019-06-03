d7ed075a371b8a46eccd55e8e68f76962d781730
Stathis Andronikos <sandronikos@ss7devsrv.amdtelecom.net>
2019-06-03 15:16:15 +0300

    Implementing Jira160, Jira161, Jira191

--- ulibss7config/API/UMSS7ApiTaskSS7FilterStagingArea_add.m
+++ ulibss7config/API/UMSS7ApiTaskSS7FilterStagingArea_add.m
@@ -10,6 +10,8 @@
 #import "UMSS7ConfigObject.h"
 #import "UMSS7ConfigSS7FilterStagingArea.h"
 #import "UMSS7ConfigStorage.h"
+#import "UMSS7ConfigAppDelegateProtocol.h"
+#import "UMSS7ApiSession.h"
 
 @implementation UMSS7ApiTaskSS7FilterStagingArea_add
 
@@ -32,6 +34,25 @@
         [self sendErrorNotAuthorized];
         return;
     }
-    [self sendErrorNotImplemented];
+    
+	// 1. Get Staging Area
+	UMSS7ConfigStagingAreaStorage *stagingArea = [_appDelegate getStagingAreaForSession:_apiSession.sessionKey];
+	if(stagingArea != NULL)
+    {
+        [self sendErrorAlreadyExisting];
+    }
+    else
+    {
+		@try
+		{
+			NSString *name = _webRequest.params[@"name"];
+			[_appDelegate createSS7FilterStagingArea:name];
+			[self sendResultOK];
+		}
+		@catch(NSException *e)
+		{
+			[self sendException:e];
+		}
+    }
 }
 @end
