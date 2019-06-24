13d4490253acd8b3d39f91be6c66d1cb93bf45aa
Andreas Fink <andreas@fink.org>
2019-06-24 10:40:17 +0200

    work in progress

--- ulibss7config/DiameterSessions/DiameterSession3GPP_UDR.m
+++ ulibss7config/DiameterSessions/DiameterSession3GPP_UDR.m
@@ -13,14 +13,66 @@
 
 - (NSString *)webTitle
 {
-    return @"3GPP-UDR";
+    return @"3GPP-UDR User Data Request";
 }
 
 - (void)webDiameterParameters:(NSMutableString *)s
 {
     [s appendString:@"<tr>\n"];
-    [s appendString:@"    <td class=optional>not-implemented</td>\n"];
-    [s appendString:@"    <td class=optional><input name=\"not-implemented\" type=text placeholder=\"+12345678\">(not-implemented))</td>\n"];
+    [s appendString:@"    <td class=optional>origin-host</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"origin-host\" value=\"default\">(local host name)</td>\n"];
+    [s appendString:@"</tr>\n"];
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>origin-realm</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"origin-realm\" value=\"default\">(local domain)</td>\n"];
+    [s appendString:@"</tr>\n"];
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>destination-host</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"destination-host\" value=\"default\">(remote host name)</td>\n"];
+    [s appendString:@"</tr>\n"];
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>destination-realm</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"destination-realm\" value=\"default\">(remote domain)</td>\n"];
+    [s appendString:@"</tr>\n"];
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>user-name</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"user-name\" type=text placeholder=\"001011234567890\">(IMSI))</td>\n"];
+    [s appendString:@"</tr>\n"];
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>rat-type</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"rat-type\"></td>\n"];
+    [s appendString:@"</tr>\n"];
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>ulr-flags</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"ulr-flags\"></td>\n"];
+    [s appendString:@"</tr>\n"];
+
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>visited-plmn-id</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"visited-plmn-id\"></td>\n"];
+    [s appendString:@"</tr>\n"];
+
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>features-flags</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"features-flags\"></td>\n"];
+    [s appendString:@"</tr>\n"];
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>terminal-information-imei</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"terminal-information-imei\"></td>\n"];
+    [s appendString:@"</tr>\n"];
+
+    [s appendString:@"<tr>\n"];
+    [s appendString:@"    <td class=optional>terminal-information-software-version</td>\n"];
+    [s appendString:@"    <td class=optional><input name=\"terminal-information-software-version\"></td>\n"];
     [s appendString:@"</tr>\n"];
 }
 
