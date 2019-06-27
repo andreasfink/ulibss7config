63a7bc1447c24b7b6b2d98aa8021748fb57737a6
Andreas Fink <afink@amdtelecom.net>
2019-06-27 17:45:04 +0300

    minor

--- ulibss7config/UMSS7TraceFile.h
+++ ulibss7config/UMSS7TraceFile.h
@@ -7,11 +7,25 @@
 //
 
 #import <ulibpcap/ulibpcap.h>
+#import <ulibgsmmap/ulibgsmmap.h>
 
+#import "UMSS7ConfigSS7FilterTraceFile.h"
 
 @interface UMSS7TraceFile : UMObject
 {
+	UMSS7ConfigSS7FilterTraceFile *_config;
 	UMPCAPFile *_pcap;
 }
+
+@property(readwrite,strong,atomic)	UMSS7ConfigSS7FilterTraceFile *config;
+
+- (void)logPacket:(UMSCCP_Packet *)packet;
+- (UMSS7TraceFile *)initWithSS7Config:(UMSS7ConfigSS7FilterTraceFile *)config;
+
+- (void)open;
+- (void)close;
+- (void)rotate;
+
+
 @end
 
