ac4744c94fe7b7dfc226aede0cc970617853044c
Andreas Fink <andreas@fink.org>
2019-10-17 09:08:19 +0200

    work in progress

--- ulibss7config/app/UMSS7App.h
+++ ulibss7config/app/UMSS7App.h
@@ -12,6 +12,8 @@
 #import <ulibtcap/ulibtcap.h>
 #import <ulibgsmmap/ulibgsmmap.h>
 #import <ulibcamel/ulibcamel.h>
+#import <ulibdiameter/ulibdiameter.h>
+@class SS7AppDelegate;
 
 typedef enum UMSS7AppType
 {
@@ -20,7 +22,7 @@ typedef enum UMSS7AppType
     UMSS7AppType_TCAP = 2,
     UMSS7AppType_GSMMAP = 3,
     UMSS7AppType_CAMEL = 4,
-//    UMSS7AppType_Diameter = 5,
+    UMSS7AppType_Diameter = 5,
 } UMSS7AppType;
 
 @interface UMSS7App : UMPlugin
@@ -29,24 +31,37 @@ typedef enum UMSS7AppType
     BOOL            _isActive;
     UMMTP3PointCode *_pc;
     NSNumber        *_mtp3_si;
+
+    SS7AppDelegate  *_appDelegate;
     SccpAddress     *_globalTitle;
+    UMLayerSCCP     *_sccp;
     UMLayerTCAP     *_tcap;
     UMLayerGSMMAP   *_map;
     UMLayerCamel    *_camel;
-//  UMLayerDiameter *_diameter;
+    UMDiameterRouter *_diameterRouter;
 }
 
 @property(readwrite,assign,atomic)      UMSS7AppType    appType;
 @property(readwrite,assign,atomic)      BOOL            isActive;
 @property(readwrite,strong,atomic)      SccpAddress     *globalTitle;
+@property(readwrite,strong,atomic)      UMLayerSCCP     *scccp;
 @property(readwrite,strong,atomic)      UMLayerTCAP     *tcap;
 @property(readwrite,strong,atomic)      UMLayerGSMMAP   *map;
 @property(readwrite,strong,atomic)      UMLayerCamel    *camel;
+@property(readwrite,strong,atomic)      UMDiameterRouter *diameterRouter;
+@property(readwrite,strong,atomic)      SS7AppDelegate  *appDelegate;
+
 //@property(readwrite,strong,atomic)      UMLayerDiameter *diameter;
 
 
-- (UMSS7App *)initWithConfig:(NSDictionary *)dict;
-- (BOOL)setConfig:(NSDictionary *)dict; /* returns YES on Success */
+- (UMSS7App *)initWithAppDelegate:(SS7AppDelegate *)appdel;
+- (int)setConfig:(NSDictionary *)dict; /* returns 0 on Success */
+
+- (int)load; /* returns 0 on success */
+- (int)createInstance; /* returns 0 on success */
+- (int)startInstance; /* returns 0 on success */
+- (int)stopInstance; /* returns 0 on success */
+- (int)unload; /* returns 0 on success */
 
 @end
 
--- ulibss7config/app/UMSS7App.m
+++ ulibss7config/app/UMSS7App.m
@@ -7,25 +7,51 @@
 //
 
 #import "UMSS7App.h"
+#import "SS7AppDelegate.h"
 
 @implementation UMSS7App
 
-- (UMSS7App *)initWithConfig:(NSDictionary *)dict
+- (UMSS7App *)initWithAppDelegate:(SS7AppDelegate *)appdel
 {
     self = [super init];
     if(self)
     {
-        if([self setConfig:dict] == NO)
-        {
-            return NULL;
-        }
+        _appDelegate = appdel;
+        /* do something here */
     }
     return self;
 }
 
-- (BOOL)setConfig:(NSDictionary *)dict /* returns YES on Success */
+
+- (int)setConfig:(NSDictionary *)dict /* returns 0 on success */
 {
-    return YES;
+    return 0;
+}
+
+
+- (int)load /* returns 0 on success */
+{
+    return 0;
+}
+
+- (int)createInstance /* returns 0 on success */
+{
+    return 0;
+}
+
+- (int)startInstance /* returns 0 on success */
+{
+    return 0;
+}
+
+- (int)stopInstance /* returns 0 on success */
+{
+    return 0;
+}
+
+- (int)unload /* returns 0 on success */
+{
+    return 0;
 }
 
 @end
