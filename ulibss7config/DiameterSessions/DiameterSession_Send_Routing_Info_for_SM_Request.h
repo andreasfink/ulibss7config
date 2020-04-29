533d0986d253881650ccea421efe4af8731bdc1b
Andreas Fink <andreas@fink.org>
2020-04-29 22:30:10 +0200

    work in progress

---
+++ ulibss7config/DiameterSessions/DiameterSession_Send_Routing_Info_for_SM_Request.h
@@ -0,0 +1,15 @@
+//
+//  DiameterSession_Send_Routing_Info_for_SM_Request.h
+//  ulibss7config
+//
+//  Created by Andreas Fink on 29.04.2020.
+//  Copyright © 2020 Andreas Fink. All rights reserved.
+//
+
+#import "DiameterGenericSession.h"
+
+
+@interface DiameterSession_Send_Routing_Info_for_SM_Request : DiameterGenericSession
+
+@end
+
---
+++ ulibss7config/DiameterSessions/DiameterSession_Send_Routing_Info_for_SM_Request.m
@@ -0,0 +1,48 @@
+//
+//  DiameterSession_Send_Routing_Info_for_SM_Request.m
+//  ulibss7config
+//
+//  Created by Andreas Fink on 29.04.2020.
+//  Copyright © 2020 Andreas Fink. All rights reserved.
+//
+
+#import "DiameterSession_Send_Routing_Info_for_SM_Request.h"
+#import "WebMacros.h"
+#import <ulibdiameter/ulibdiameter.h>
+
+@implementation DiameterSession_Send_Routing_Info_for_SM_Request
+
+- (NSString *)webTitle
+{
+    return @"Send-Routing-Info-for-SM-Request";
+}
+
+- (NSString *)webScript
+{
+    return [NSString stringWithFormat:@"    <script>const vars = %@</script>\n" , [UMDiameterPacketSend_Routing_Info_for_SM_Request webJsonDefintion]];
+}
+
+
+- (void)webDiameterParameters:(NSMutableString *)s
+{
+    [self webApplicationParameters:s defaultApplicationId:[UMDiameterPacketSend_Routing_Info_for_SM_Request defaultApplicationId] comment:NULL];
+    [UMDiameterPacketUser_Data_Request webDiameterParameters:s];
+}
+
+- (void)main
+{
+    @try
+    {
+        UMDiameterPacketUser_Data_Request *pkt = [[UMDiameterPacketUser_Data_Request alloc]init];
+        [pkt setDictionaryValueFromWeb:_req.params];
+        self.query = pkt;
+        [self submit];
+    }
+    @catch(NSException *e)
+    {
+        [self webException:e];
+    }
+}
+
+@end
+
