dd1bb85b8f8e8cbc62ece89738a541e388838066
Andreas Fink <andreas@fink.org>
2020-07-22 17:43:38 +0200

    fix imsi prefix length restrictions

--- ulibss7config/SS7TemporaryImsiPool.m
+++ ulibss7config/SS7TemporaryImsiPool.m
@@ -60,9 +60,9 @@
             [_unusedTemporaryImsis append:[NSString stringWithFormat:@"00101999%07d",i]];
         }
     }
-    else if((s.length < 7) || (s.length >15))
+    else if((s.length < 5) || (s.length >15))
     {
-        @throw(CONFIG_ERROR(@"imsi-prefix should be 7 to 15 digits"));
+        @throw(CONFIG_ERROR(@"imsi-prefix should be 5 to 15 digits"));
     }
     else
     {
