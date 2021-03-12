938420309708f3b0c078dbc8fafb9da5f87c0ce8
Andreas Fink <andreas@fink.org>
2021-03-12 09:53:29 +0100

    wip

--- ulibss7config/Config/UMSS7ConfigDiameterRouter.m
+++ ulibss7config/Config/UMSS7ConfigDiameterRouter.m
@@ -37,6 +37,17 @@
     [super appendConfigToString:s];
     APPEND_CONFIG_STRING(s,@"local-hostname",_localHostName);
     APPEND_CONFIG_STRING(s,@"local-realm",_localRealm);
+    
+    APPEND_CONFIG_STRING(s,@"statistic-db-pool",_statisticDbPool);
+    APPEND_CONFIG_STRING(s,@"statistic-db-table",_statisticDbTable);
+    APPEND_CONFIG_STRING(s,@"statistic-db-instance",_statisticDbInstance);
+    APPEND_CONFIG_BOOLEAN(s,@"statistic-db-autocreate",_statisticDbAutocreate);
+    
+    APPEND_CONFIG_STRING(s,@"screening-sccp-plugin-name",_screeningDiameterPluginName);
+    APPEND_CONFIG_STRING(s,@"screening-sccp-plugin-config-file",_screeningDiameterPluginConfigFile);
+    APPEND_CONFIG_STRING(s,@"screening-sccp-plugin-trace-file",_screeningDiameterPluginTraceFile);
+
+
 }
 
 
@@ -45,6 +56,13 @@
     UMSynchronizedSortedDictionary *dict = [super config];
     APPEND_DICT_STRING(dict,@"local-hostname",_localHostName);
     APPEND_DICT_STRING(dict,@"local-realm",_localRealm);
+    APPEND_DICT_STRING(dict,@"statistic-db-pool",_statisticDbPool);
+    APPEND_DICT_STRING(dict,@"statistic-db-table",_statisticDbTable);
+    APPEND_DICT_STRING(dict,@"statistic-db-instance",_statisticDbInstance);
+    APPEND_DICT_BOOLEAN(dict,@"statistic-db-autocreate",_statisticDbAutocreate);
+    APPEND_DICT_STRING(dict,@"screening-diameter-plugin-name",_screeningDiameterPluginName);
+    APPEND_DICT_STRING(dict,@"screening-diameter-plugin-config-file",_screeningDiameterPluginConfigFile);
+    APPEND_DICT_STRING(dict,@"screening-diameter-plugin-trace-file",_screeningDiameterPluginTraceFile);
 
     return dict;
 }
@@ -54,7 +72,14 @@
     [self setSuperConfig:dict];
     SET_DICT_STRING(dict,@"local-hostname",_localHostName);
     SET_DICT_STRING(dict,@"local-realm",_localRealm);
+    SET_DICT_STRING(dict,@"statistic-db-pool",_statisticDbPool);
+    SET_DICT_STRING(dict,@"statistic-db-table",_statisticDbTable);
+    SET_DICT_STRING(dict,@"statistic-db-instance",_statisticDbInstance);
+    SET_DICT_BOOLEAN(dict,@"statistic-db-autocreate",_statisticDbAutocreate);
 
+    SET_DICT_STRING(dict,@"screening-diameter-plugin-name",_screeningDiameterPluginName);
+    SET_DICT_STRING(dict,@"screening-diameter-plugin-config-file",_screeningDiameterPluginConfigFile);
+    SET_DICT_STRING(dict,@"screening-diameter-plugin-trace-file",_screeningDiameterPluginTraceFile);
 
 }
 
