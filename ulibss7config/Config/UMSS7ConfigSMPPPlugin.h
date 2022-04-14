1d43508cc59a971de7ce9a94209ac1392d2d3257
Andreas Fink <andreas@fink.org>
2022-04-14 20:42:52 +0300

    fix wrong status display if link comes up and SLTM is received while we are in status READY and not IS yet. We miss the status update in this case.

---
+++ ulibss7config/Config/UMSS7ConfigSMPPPlugin.h
@@ -0,0 +1,25 @@
+//
+//  UMSS7ConfigSMPPPlugin.h
+//  ulibss7config
+//
+//  Created by Andreas Fink on 11.04.22.
+//  Copyright © 2022 Andreas Fink. All rights reserved.
+//
+
+#import <ulib/ulib.h>
+#import "UMSS7ConfigObject.h"
+
+@interface UMSS7ConfigSMPPPlugin : UMSS7ConfigObject
+{
+    NSString    *_pluginFileName;
+    NSString    *_configFile;
+    NSString    *_configString;
+}
+
+
+@property(readwrite,strong,atomic)  NSString    *pluginFileName;
+@property(readwrite,strong,atomic)  NSString    *configFile;
+@property(readwrite,strong,atomic)  NSString    *configString;
+
+@end
+
