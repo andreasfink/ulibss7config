d14fb43679efd8681699db96893c52168a090304
Andreas Fink <andreas@fink.org>
2023-04-14 11:22:16 +0200

    wip

--- ulibss7config/SS7AppDelegate.h
+++ ulibss7config/SS7AppDelegate.h
@@ -226,7 +226,7 @@ UMEnvironmentNamedListProviderProtocol>
 @property(readwrite,strong)     UMLogHandler    *logHandler;
 @property(readwrite,assign)     BOOL            startInStandby;
 
-@property(readonly,assign)     NSString        *hostname;
+@property(readonly,strong)      NSString            *hostname;
 
 @property(readwrite,strong)     NSDictionary		*enabledOptions;
 @property(readwrite,strong)     UMCommandLine		*commandLine;
